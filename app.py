b557038e80ac772aaa823fbe47c9ec430b4d967d
Amala-Sebastian <91590353+Amala-Sebastian@users.noreply.github.com>
2024-01-04 07:48:57 +0000

    app.py

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import numpy as np
 import  streamlit as st 
 
 
-#model=tf.keras.models.load_model("/content/drive/MyDrive/DeepLearning/my_model.keras")
-#load=pickle.load(open('train_model.sav','rb'))
-#up=st.file_uploader("hdhf",type="jpg")
+
 st.title("FAKE NEWS DETECTION")
 input_text=st.text_input("Enter news article")
