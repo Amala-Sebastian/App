a1583a21f640bb015836536b721dee3c9d1b576f
Amala-Sebastian <91590353+Amala-Sebastian@users.noreply.github.com>
2024-01-04 07:06:16 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import cv2
+import pickle
+import numpy as np
+import  streamlit as st 
+#import tensorflow as tf
+#from tensorflow.keras.preprocessing.text import Tokenizer
+#from tensorflow.keras.preprocessing.sequence import pad_sequences
+#from tensorflow.keras.models import Sequential
+#from tensorflow.keras.layers import Dense, Dropout, Embedding, GRU, LSTM, RNN, SpatialDropout1D
+#import tensorflow as tf
+
+#model=tf.keras.models.load_model("/content/drive/MyDrive/DeepLearning/my_model.keras")
+load=pickle.load(open('train_model.sav','rb'))
+#up=st.file_uploader("hdhf",type="jpg")
+st.title("FAKE NEWS DETECTION")
+input_text=st.text_input("Enter news article")
