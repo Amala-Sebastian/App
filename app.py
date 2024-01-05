8e8e5dbc392f87498709b0a3fc00c756c05c59f6
Amala-Sebastian <91590353+Amala-Sebastian@users.noreply.github.com>
2024-01-05 03:21:26 +0000

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 import  streamlit as st 
 
-import tensorflow as tf
-load=pickle.load(open('train_model.sav','rb'))
+
+
 st.title("FAKE NEWS DETECTION")
 input_text=st.text_input("Enter news article")
